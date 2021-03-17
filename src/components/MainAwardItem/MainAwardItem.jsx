b381615869dbde93b6e1109dd325c17c091ec40c
편도걸 <pdg2491@naver.com>
2021-03-17 23:11:26 +0900

    fix code cleanup
    - MainAwardItem Component
    - Assets Image
    - Common Component

--- src/components/MainAwardItem/MainAwardItem.jsx
+++ src/components/MainAwardItem/MainAwardItem.jsx
@@ -1,23 +1,13 @@
 import React, { useContext } from 'react';
 import styles from './MainAwardItem.module.scss';
 import MainAwardContext from '../../context/MainAward.context';
-import Image from '../common/Image';
 
 function MainAwardItem() {
-  const { img, text } = useContext(MainAwardContext);
-  // const [mainAwardItemRef, setMainAwardItemRef] = useState(() => createRef());
-  // const combineClassName = [styles.main_award__item];
-  // if (isFade) {
-  //   combineClassName.push(styles.fade_base);
-  //   useEffect(() => {
-  //     mainAwardItemRef.current.classList.add(styles.fade_active);
-  //     return () => {};
-  //   });
-  // }
+  const { img = {}, text = '' } = useContext(MainAwardContext);
 
   return (
     <div className={styles.main_award__item}>
-      <Image {...img} />
+      <img {...img} />
       <span>{text}</span>
     </div>
   );
--- src/components/common/Image.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Image(attr) {
-  const { className, ...otherAttrs } = attr;
-  return <img {...otherAttrs} />;
-}
-
-export default Image;
